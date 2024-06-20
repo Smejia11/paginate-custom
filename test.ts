e61ad46004a2cf2ab31bf492ec7d691236dd6bf8
smejia-1 <santiago.mejia.oquendo1@gmail.com>
2024-06-20 14:02:17 -0500

    feature: add new test for method async getPaginateAsync

--- test.ts
+++ test.ts
@@ -34,3 +34,36 @@ describe("paginate unit tests", () => {
     );
   });
 });
+
+describe("paginate unit tests async", () => {
+  const data = Array.from({ length: 100 }, (_, i) => i + 1);
+
+  it("should be possible paginate an array", async () => {
+    const paginate = new PaginateUtils();
+    const { pagination } = await paginate.getPaginateAsync({
+      data,
+      page: 1,
+      limit: 10,
+      url: "http://localhost:9087",
+    });
+
+    assert.equal(
+      JSON.stringify(pagination),
+      JSON.stringify({
+        totalPage: 10,
+        nextPage: 2,
+        prevPage: null,
+        firstPage: 1,
+        lastPage: 10,
+        from: 1,
+        to: 10,
+        perPage: 10,
+        total: 100,
+        currentPage: 1,
+        hasPrevPage: false,
+        hasNextPage: true,
+        url: "http://localhost:9087?page=1",
+      })
+    );
+  });
+});
